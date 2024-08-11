60c9736dcf03b2f86b66c3ae2ff5bbf2aaa182fb
Kuldeep Bhatia <kuldeep_bhatia03@icloud.com>
2024-08-11 22:23:59 +1000

    spring boot initialised

---
+++ event-registration/src/main/java/com/eventcorp/eventregistration/EventRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.eventcorp.eventregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ event-registration/src/test/java/com/eventcorp/eventregistration/EventRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventcorp.eventregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
